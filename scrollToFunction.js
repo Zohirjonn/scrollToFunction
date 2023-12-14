037d512f6252cf97c124af9be9935c532b6383a5
Zohirjonn <120003182+Zohirjonn@users.noreply.github.com>
2023-12-14 17:12:03 +0500

    Update scrollToFunction.js

--- scrollToFunction.js
+++ scrollToFunction.js
@@ -2,3 +2,6 @@
 			const item = document.querySelector('.connect')
 			item.scrollIntoView({ behavior: 'smooth', block: 'center' })
 		}
+const filteredByKey = Object.fromEntries(
+    Object.entries(romNumbers).filter(([key, value]) => key === 'I') )
+// filteredByKey = {I: 1} 
